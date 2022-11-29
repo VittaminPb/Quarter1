e11db9c3d0c13cbdc9e9ac8e4c545b37b3b5a929
Vitaliy <vitaliy.klimov@mail.ru>
2022-11-30 00:03:05 +0300

    String array created

--- TaskC#/Program.cs
+++ TaskC#/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите значения элементов массива строк через пробел: ");
+string[] stringArray = Console.ReadLine().Split().ToArray();
+
+
+
+void PrintArray(string[] stringArr)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (i < (stringArray.Length - 1)) Console.Write($"{stringArray[i]}, ");
+        else Console.Write($"{stringArray[i]}");
+    }
+    Console.Write("]");
+}
+
+PrintArray(stringArray);
