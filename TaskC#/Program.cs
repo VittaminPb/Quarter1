3c5b98e588b65cac8acab1d29a878f08fdbd9a35
Vitaliy <vitaliy.klimov@mail.ru>
2022-11-30 21:12:06 +0300

    Added SortArray method

--- TaskC#/Program.cs
+++ TaskC#/Program.cs
@@ -1,8 +1,6 @@
 ﻿Console.Write("Введите значения элементов массива строк через пробел: ");
 string[] stringArray = Console.ReadLine().Split().ToArray();
 
-
-
 void PrintArray(string[] stringArr)
 {
     Console.Write("[");
@@ -14,4 +12,21 @@ void PrintArray(string[] stringArr)
     Console.Write("]");
 }
 
-PrintArray(stringArray);
+string [] SortArray(string[] stringArr)
+{
+    int k = 0;
+    string[] sortArray = new string[stringArray.Length];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            sortArray[k] = stringArray[i];
+            k++;
+        }
+    }
+    Array.Resize(ref sortArray, k);
+    return sortArray;
+}
+
+string[] sortArray = SortArray();
+PrintArray(sortArray);
